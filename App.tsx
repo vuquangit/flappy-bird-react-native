1566edbdb6c7f14f66332fc04fa53c8af0116f3d
vuquangit <vv.quang@1pac.vn>
2021-06-23 01:05:10 +0700

    config project and create components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
-export default function App() {
+import HomeScreen from './src/screens/Home'
+
+const App: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <HomeScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      backgroundColor: '#fff',
   },
-});
+})
+
+export default App
